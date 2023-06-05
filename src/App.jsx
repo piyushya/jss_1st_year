58f174c26cc96e04f197d2ac75d1d37dc42d4dbe
Piyush Yadav <piyushya012@gmail.com>
2023-06-06 01:16:28 +0530

    styled

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import {db} from '../firebase'
 import { getDoc, updateDoc, doc, collection } from 'firebase/firestore'
 // import { reset, addUsers} from './dbOps'
 import {sections, branches, studentTemplate} from './data'
+import JSConfetti from 'js-confetti'
 import studentsList from './assets/students_List.json'
 import './App.css'
 // import { addUsers } from './dbOps';
+
+const body = document.querySelector("body")
 
 export default function App() {
   const [studentData, setStudentData] = React.useState({
@@ -14,6 +17,7 @@ export default function App() {
   })
 
   const [Options, setOptions] = React.useState([])
+  const [level, setLevel] = React.useState("1")
 
   const [liked, setLiked] = React.useState(localStorage.getItem(studentData.roll_no) ? 
   localStorage.getItem(studentData.roll_no) : "0")
@@ -22,10 +26,28 @@ export default function App() {
 
   // get likes from database and get liked status from local storage if 
   // the current user is changed from select dropdown
+  const confettiData = {
+    emojis : ["❤️"],
+    emojiSize : `${(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))?120:150}`,
+    confettiNumber : 15
+  }
+  const developerData = {
+    emojis : ["👨‍💻", "💻", "🤖"],
+    emojiSize : `${(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))?120:150}`,
+    confettiNumber : 20
+  }
+
+  const jsConfetti = new JSConfetti()
   React.useEffect(() => {
+    if(studentData.gender === "female"){
+      body.classList.add("female")
+    }else{
+      body.classList.remove("female")
+    }
     get_Likes(studentData.roll_no)
     setLiked(localStorage.getItem(studentData.roll_no) ? 
     localStorage.getItem(studentData.roll_no) : "0")
+    console.log("data loaded")
   }, [studentData])
 
   // set names of students to display in the dropdown
@@ -39,6 +61,26 @@ export default function App() {
     setOptions(localOptions)
     console.log("Options set for dropdown")
   }, [])
+
+  React.useEffect(()=>{
+    console.log(likes)
+    if(likes >= 50){
+      setLevel("4")
+      body.className = "body4"
+    }
+    else if(likes >= 30){
+      setLevel("3")
+      body.className = "body3"
+    }
+    else if(likes >= 10){
+      setLevel("2")
+      body.className = "body2"
+    }
+    else{
+      setLevel("1")
+      body.className = "body1"
+    }
+  }, [likes])
 
   // get student data of the chosen student
   function getStudentData(roll_no){
@@ -92,6 +134,7 @@ export default function App() {
   }
 
   function handleLike(){
+    jsConfetti.addConfetti(confettiData)
     if(liked === "1"){
       setLiked("0")
       localStorage.setItem(studentData.roll_no, "0")
@@ -104,15 +147,29 @@ export default function App() {
     }
   }
 
+  function goToDiscord(){
+    window.open("#", "_blank")
+  }
+
+  function goToDeveloper(){
+    jsConfetti.addConfetti(developerData)
+    setStudentData(getStudentData("22CS142"))
+    window.scrollTo(0, 0)
+  }
+
   return (
-    <div className={`page ${studentData.gender==="female"?"female":""}`}>
+    <div>
       <nav className='navigator'>
           <Select
             className='selectBox'
             onChange={handleSearch}
             options={Options}
           />
-          {storageAvailable("localStorage") && 
+          {studentData.name && <div className={`deskDiscordInfo deskDiscordInfo${level}`} onClick={goToDiscord}>
+            <img className='deskDiscordLink' src="/discord.png" alt="discord logo"/>
+            <p className='deskDiscordText'>Join our Discord Server and hangout on variour channels with your JSS mates</p>
+          </div>}
+          {storageAvailable("localStorage") && studentData.name &&
           <button className='heart' onClick={handleLike} type='button'>
             <img className="heartLogo" src = {(liked==="1") ? "/heart-solid.svg" : "/heart-regular.svg"}/>
             <span className={liked==="1" ? 'likeCount likeCountLiked' : "likeCount"}> {likes} </span>
@@ -120,27 +177,48 @@ export default function App() {
       </nav>
       {studentData.name ?
       <main className='content'>
-        <div className='std_name'>{studentData.name}</div>
+        <div className={`stdName stdName${level} std${studentData.gender}`}>
+          {studentData.name}
+          <img className="badge" src={`/badges/${level}.png`} alt="level badge"/>
+        </div>
         <div className='std_branch phone_view'>{studentData.branch}</div>
         <div className='infoContainer'>
           <div className='infoRow'>
-            <div className='infoBox'>Section {studentData.section}</div>
-            <div className='infoBox'>Group {studentData.group}</div>
+            <div className={`infoBox infoBox${level}`}>Section {studentData.section}</div>
+            <div className={`infoBox infoBox${level}`}>Group {studentData.group}</div>
           </div>
-          <div className='std_branch desk_view'>{studentData.branch}</div>
+          <div className={`std_branch desk_view${level} desk_view`}>{studentData.branch}</div>
           <div className='infoRow'>
-            <div className='infoBox'>{studentData.roll_no}</div>
-            <div className='infoBox'>Gender {studentData.gender}</div>
+            <div className={`infoBox infoBox${level}`}>{studentData.roll_no}</div>
+            <div className={`infoBox infoBox${level}`}>Gender {studentData.gender}</div>
           </div>
+        </div>
+        <div className='Info discordInfo phoneDiscordInfo'>
+          <a href="#"><img className='discordLink' src="/discord.png" alt="discord logo"/></a>
+          <p className='infoText'>Join our Discord Server and hangout on variour channels with your JSS mates</p>
         </div>
       </main>
       : 
       <main className='notSearched'>
-        <p>Search For a Student to show his/her details</p>
+        <h1 className='mainInfo'>Search for your friends and give them a ❤️</h1>
+        <div className='infoContainerMain'>
+          <div className='Info discordInfo'>
+            <a href="#"><img className='discordLink' src="/discord.png" alt="discord logo"/></a>
+            <p className='infoText'>Also join our Discord Server and hangout on variour channels with your JSS mates</p>
+          </div>
+          <div className='Info levelInfo'>
+            <p className='infoText'>
+              0-9 ❤️ : level 1 <img width="30px" src='/badges/1.png'/><br/>
+              10-29 ❤️ : level 2 <img width="30px" src='/badges/2.png'/><br/>
+              30-49 ❤️ : level 3 <img width="30px" src='/badges/3.png'/><br/>
+              50--- ❤️ : level 4 <img width="30px" src='/badges/4.png'/>
+            </p>
+          </div>
+        </div>
       </main>}
-      {/* <footer className='bottom'>
-        <p>cc 2023</p>
-      </footer> */}
+      <footer className='footer' onClick={goToDeveloper}>
+        <p>Made with ❤️ by Piyush</p>
+      </footer>
     </div>
   )
 }
